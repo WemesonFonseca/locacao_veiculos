67da5557035cbb8c0796e76bfea7282a0de27faa
WemesonFonseca <wemessoncarloss16@gmail.com>
2023-10-24 15:52:29 -0300

    Update locacao_carros.sql
    Correção no banco de dados.

--- locacao_carros.sql
+++ locacao_carros.sql
@@ -1,26 +1,27 @@
 create database Locacao_carros;
 
-use locacao_carros;
+use Locacao_carros;
 
 /*Tabela Usuário*/
-create table Usu_Usuario
+create table USU_Usuario
 (usu_cod int not null auto_increment,
 nome_usuario varchar(45) null,
 senha_usuario varchar(45) null,
 email_usuario varchar(45) not null,
 primary key(usu_cod));
 
+
 /*Tabela Carro*/
 create table CAR_Carro
 (car_cod int not null auto_increment,
 car_placa varchar(8) not null,
 car_desc varchar(45),
-data_aluguel date null,
 primary key (car_cod), 
 unique key (car_placa));
 
+
 /*Inserindo dados dos carros, (Obs: dados fictícios)*/
-insert into CAR_carro values 
+insert into CAR_Carro values 
 ('1', 'JGO-3055', 'Compacto Plus'),
 ('2', 'JKF-0629', 'Sedan Econômico'),
 ('3', 'JHI-2649', 'Minivan'),
@@ -31,15 +32,15 @@ insert into CAR_carro values
 ('8', 'JKE-4598', 'Caminhonete 4x4'),
 ('9', 'JGQ-3342', 'Forgão');
 
-/*Tabela histórico*/
-CREATE TABLE his_historico (
-    his_id INT NOT NULL AUTO_INCREMENT,
-    car_cod INT NOT NULL,
-    usu_cod INT,
-    car_placa varchar(8),
-    car_desc VARCHAR(45),
-    data_aluguel DATE NOT NULL,
-    PRIMARY KEY (his_id),
-	FOREIGN KEY (usu_cod) REFERENCES usu_usuario(usu_cod),
-    FOREIGN KEY (car_cod) REFERENCES CAR_Carro(car_cod));
 
+/*Tabela histórico*/
+create table HIS_Historico (
+his_id int not null auto_increment,
+car_cod int not null,
+usu_cod int not null,
+car_placa varchar(8),
+car_desc varchar(45),
+data_aluguel date not null,
+primary key (his_id),
+foreign key (usu_cod) references USU_Usuario(usu_cod),
+foreign key (car_cod) references CAR_Carro(car_cod));
